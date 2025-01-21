ad82a2782c1e96c6482472bb0264bff04d0b70e7
Hyojin-Moon <moontough8@gmail.com>
2025-01-21 16:04:49 +0900

    ✨ Feat: state로 초기상태 설정 handler함수생성
    state 및 handler 함수 생성

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import './App.css'
 
 function App() {
 
+  // 기본 상태를 관리할 useState 훅을 사용해 초기 상태를 설정
+  const [data, setData] = useState('');
+  const handleInputChange = () => {
+  }
+  //onSubmit 이벤트 핸들러를 설정
+  const handleSubmit = () => {
+
+  }
+
   return (
     <>
       <div className='main-container'>
@@ -10,31 +19,31 @@ function App() {
           <h1>2025 Olympics</h1>
         </nav>
 
-        <div className='input-section'>
+        <form className='input-section' onSubmit={handleSubmit}> {/*onSubmit 사용 form으로*/}
 
           <div className="input-group">
             <p>국가명</p>
-            <input type="text" id="nation" placeholder="국가 입력" />
+            <input type="text" id="nation" placeholder="국가 입력" onChange={handleInputChange} />
           </div>
-          <div className="input-group">
+          <div className="input-group" >
             <p>금메달</p>
-            <input type="text" id="gold" />
+            <input type="text" id="gold" onChange={handleInputChange} />
           </div>
           <div className="input-group">
             <p>은메달</p>
-            <input type="text" id="silver" />
+            <input type="text" id="silver" onChange={handleInputChange} />
           </div>
           <div className="input-group">
             <p>동메달</p>
-            <input type="text" id="bronze" />
+            <input type="text" id="bronze" onChange={handleInputChange} />
           </div>
 
           <div className='button-section'>
-            <input type="button" id='addbtn' value="국가 추가" />
-            <input type="button" id='updatebtn' value="업데이트" />
+            <input type='submit' id='addbtn' value="국가 추가" />
+            <input type="submit" id='updatebtn' value="업데이트" />
           </div>
 
-        </div>
+        </form>
 
         <h3>*입력된 값이 들어올 구역*</h3>
 
