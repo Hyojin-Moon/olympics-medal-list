f4b7e8876a67972e45e990f0ae81220677abe2f4
Hyojin-Moon <moontough8@gmail.com>
2025-01-21 15:17:13 +0900

    🦺 Add : html 구조 생
    html 제목과 입력칸 버튼 생성

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='main-container'>
+        <nav className='navbar'>
+          <h1>2025 Olympics</h1>
+        </nav>
+
+        <div className='input-section'>
+
+          <div className="input-group">
+            <p>국가명</p>
+            <input type="text" id="nation" placeholder="국가 입력" />
+          </div>
+          <div className="input-group">
+            <p>금메달</p>
+            <input type="text" id="gold" />
+          </div>
+          <div className="input-group">
+            <p>은메달</p>
+            <input type="text" id="silver" />
+          </div>
+          <div className="input-group">
+            <p>동메달</p>
+            <input type="text" id="bronze" />
+          </div>
+
+
+          <div className='button-section'>
+            <input type="button" id='addbtn' value="국가 추가" />
+            <input type="button" id='updatebtn' value="업데이트" />
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
