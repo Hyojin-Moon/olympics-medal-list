4969460169ec8c93b519a01c5ff2317a52876472
Hyojin-Moon <moontough8@gmail.com>
2025-01-22 10:34:35 +0900

    ♻️ Feat: 추가리스트 헤더/리스트로 나눔
    헤더는 한번만 나오면 되므로 영역을 나눠주고 1개이상일 경우에만 나오도록 설정 , 하나도없으면 메세지 보여줌

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,20 @@ function App() {
 
 
   const handleInputChange = (e) => {
-    const {id, value} = e.target;
+    const { id, value } = e.target;
     //메달 input은 0이상의 숫자만 허용
     if ((id === 'gold' || id === 'silver' || id === 'bronze') && (isNaN(value) || value < 0)) {
       return;
     }
     // 계산된 속성 이름 (Computed Property Name) id 변수의 키값을 동적으로 가져옴
-    setData({...data, [id]:value}); 
+    setData({ ...data, [id]: value });
   }
 
   //onClick 이벤트 핸들러를 설정
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    setNations([...nations, data]) //추가될 리스트
+
+    setNations([...nations, data]) //추가될 리스트 
 
     setData({
       nation: '',
@@ -36,7 +36,7 @@ function App() {
       bronze: '',
     });
   }
-
+  console.log(nations);
   return (
     <>
       <div className='main-container'>
@@ -44,46 +44,58 @@ function App() {
           <h1>2025 Olympics</h1>
         </nav>
 
-        <form className='input-section' > 
-
+        <form className='input-section' onSubmit={handleSubmit} >
+          {/* 입력 섹션 */}
           <div className="input-group">
             <p>국가명</p>
-            <input type="text" id="nation" placeholder="국가 입력" onChange={handleInputChange} value={data.nation}/>
+            <input type="text" id="nation" placeholder="국가 입력" onChange={handleInputChange} value={data.nation} />
           </div>
           <div className="input-group" >
             <p>금메달</p>
-            <input type="text" id="gold" onChange={handleInputChange} value={data.gold}/>
+            <input type="text" id="gold" onChange={handleInputChange} value={data.gold} />
           </div>
           <div className="input-group">
             <p>은메달</p>
-            <input type="text" id="silver" onChange={handleInputChange} value={data.silver}/>
+            <input type="text" id="silver" onChange={handleInputChange} value={data.silver} />
           </div>
           <div className="input-group">
             <p>동메달</p>
-            <input type="text" id="bronze" onChange={handleInputChange} value={data.bronze}/>
+            <input type="text" id="bronze" onChange={handleInputChange} value={data.bronze} />
           </div>
-
+          {/* 버튼 섹션 */}
           <div className='button-section'>
-            <input type='submit' id='addbtn' value="국가 추가" onClick={handleSubmit}/>
+            <input type='submit' id='addbtn' value="국가 추가" />
             <input type="submit" id='updatebtn' value="업데이트" />
           </div>
 
         </form>
-         {/* 국가리스트 */}
+        {/* 국가리스트 추가 */}
         <div className='nations-list'>
-          {nations.map((nation, index) => (
-            <div key={index} className='nations-item'>
-              <p>국가명: {nation.nation}</p>
-              <p>금메달: {nation.gold}</p>
-              <p>은메달: {nation.silver}</p>
-              <p>동메달: {nation.bronze}</p>
-            </div>
-          ))}
+          {nations.length > 0 && (
+            <>
+              {/* 헤더 */}
+              <div className='nations-header'>
+                <p>국가명</p>
+                <p>금메달</p>
+                <p>은메달</p>
+                <p>동메달</p>
+              </div>
+              {/* 리스트 */}
+              {nations.map((nation, index) => (
+                <div key={index} className='nations-item'>
+                  <p>{nation.nation}</p>
+                  <p>{nation.gold}</p>
+                  <p>{nation.silver}</p>
+                  <p>{nation.bronze}</p>
+                </div>
+              ))}
+            </>
+          )}
+          {nations.length === 0 && <p>현재 추가된 국가가 없습니다.</p>}
         </div>
-
       </div>
     </>
   )
 }
 
-export default App
+export default App;
